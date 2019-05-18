c80cfdc4dcf6f794623cc0b1e0fa7ea65734b55c
alan.cramer <alan.cramer@hmhco.com>
2019-05-18 10:50:08 -0400

    add start of draw function

--- fourier/drawWaveform.js
+++ fourier/drawWaveform.js
@@ -4,17 +4,16 @@ let createWaveformDom = function(svgg, wfpts) {
         .enter()
             .append('circle')
             .attr('class', 'wfpt')
-            .attr('r',  d => 3)
+            .attr('r',  d => 1)
             .attr('cx', d => scale(d.x))
             .attr('cy', d => scale(d.y))
-
 }
 
 let updateWaveformDom = function(svgg, wfpts) {
     svgg.selectAll('.wfpt')
         .data(wfpts)
         .join('.wfpt')
-            .attr('r',  d => 3)
+            .attr('r',  d => 1)
             .attr('cx', d => scale(d.x))
             .attr('cy', d => scale(d.y))
 
